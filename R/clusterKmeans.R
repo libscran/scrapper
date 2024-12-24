301e4a371b7753c71daf76c7af490fd2c8aa2cc0
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-12-24 01:17:42 -0800

    Cleaned up the k-means documentation.

--- R/clusterKmeans.R
+++ R/clusterKmeans.R
@@ -1,6 +1,6 @@
 #' K-means clustering
 #'
-#' Perform k-means clustering using kmeans++ initialization with the Hartigan-Wong algorithm. 
+#' Perform k-means clustering with a variety of different initialization and refinement algorithms.
 #'
 #' @param x Numeric matrix where rows are dimensions and columns are cells.
 #' @param k Integer scalar specifying the number of clusters.
@@ -9,14 +9,15 @@
 #' @param refine.method String specifying the refinement method:
 #' Lloyd's algorithm (\code{"lloyd"}) or the Hartigan-Wong algorithm (\code{"hartigan-wong"}).
 #' @param var.part.optimize.partition Logical scalar indicating whether each partition boundary should be optimized to reduce the sum of squares in the child partitions.
+#' Only used if \code{init.method = "var.part"}.
 #' @param var.part.size.adjustment Numeric scalar between 0 and 1, specifying the adjustment to the cluster size when prioritizing the next cluster to partition.
 #' Setting this to 0 will ignore the cluster size while setting this to 1 will generally favor larger clusters.
+#' Only used if \code{init.method = "var.part"}.
 #' @param lloyd.iterations Integer scalar specifying the maximmum number of iterations for the Lloyd algorithm.
 #' @param hartigan.wong.iterations Integer scalar specifying the maximmum number of iterations for the Hartigan-Wong algorithm.
 #' @param hartigan.wong.quick.transfer.iterations Integer scalar specifying the maximmum number of quick transfer iterations for the Hartigan-Wong algorithm.
 #' @param hartigan.wong.quit.quick.transfer.failure Logical scalar indicating whether to quit the Hartigan-Wong algorithm upon convergence failure during quick transfer iterations.
 #' @param seed Integer scalar specifying the seed to use for random or kmeans++ initialization.
-#' 
 #' @param num.threads Integer scalar specifying the number of threads to use.
 #' 
 #' @return 
@@ -24,7 +25,7 @@
 #' \itemize{
 #' \item \code{clusters}, a factor containing the cluster assignments for each cell.
 #' \item \code{centers}, a numeric matrix with the coordinates of the cluster centroids (dimensions in rows, centers in columns).
-#' \item \code{iterations}, an integer scalar specifying the number of refinement iterations that were performed 
+#' \item \code{iterations}, an integer scalar specifying the number of refinement iterations that were performed.
 #' \item \code{status}, an integer scalar specifying the convergence status.
 #' Any non-zero value indicates a convergence failure though the exact meaning depends on the choice of \code{refine.method}.
 #' }
