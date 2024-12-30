1222e75c74945cc4da9b8572100faa6755398668
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-12-29 17:46:09 -0800

    Minor reorganization of the subsample_by_neighbors C++ code.

--- src/subsample_by_neighbors.cpp
+++ src/subsample_by_neighbors.cpp
@@ -6,17 +6,18 @@
 
 //[[Rcpp::export(rng=false)]]
 SEXP subsample_by_neighbors(Rcpp::IntegerMatrix indices, Rcpp::NumericMatrix distances, int min_remaining) {
-    if (indices.rows() != distances.rows() || indices.cols() != distances.cols()) {
+    int num_obs = distances.cols();
+    size_t k = distances.rows();
+    if (static_cast<size_t>(indices.rows()) != k || static_cast<int>(indices.cols()) != num_obs) {
         throw std::runtime_error("'indices' and 'distances' must have the same dimensions");
+    }
+
+    if (k < static_cast<size_t>(min_remaining)) {
+        throw std::runtime_error("'min_remaining' should not be greater than the number of neighbors");
     }
 
     const int* iptr = indices.begin();
     const double* dptr = distances.begin();
-
-    size_t k = distances.rows();
-    if (k < static_cast<size_t>(min_remaining)) {
-        throw std::runtime_error("'min_remaining' should not be greater than the number of neighbors");
-    }
 
     nenesub::Options opt;
     opt.min_remaining = min_remaining;
