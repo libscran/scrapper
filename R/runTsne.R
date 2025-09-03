3b5b24497ae759977b0a902678b4a3bfe1fd2ce6
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-09-03 15:09:38 -0700

    Added more detail to the t-SNE parameters.

--- R/runTsne.R
+++ R/runTsne.R
@@ -13,15 +13,18 @@
 #'
 #' Alternatively, an index constructed by \code{\link[BiocNeighbors]{buildIndex}}.
 #' @param perplexity Numeric scalar specifying the perplexity to use in the t-SNE algorithm.
+#' Higher perplexities will focus on global structure, at the cost of increased runtime and decreased local resolution.
 #' @param num.neighbors Integer scalar specifying the number of neighbors, typically derived from \code{perplexity}.
 #' @param max.depth Integer scalar specifying the maximum depth of the Barnes-Hut quadtree.
-#' Smaller values (7-10) improve speed at the cost of accuracy.
-#' @param leaf.approximation Logical scalar indicating whether to use the \dQuote{leaf approximation} approach,
-#' which sacrifices some accuracy for greater speed.
-#' Only effective when \code{max.depth} is small enough for multiple cells to be assigned to the same leaf node of the quadtree.
+#' If neighboring cells cannot be separated before the maximum depth is reached, they will be assigned to the same leaf node of the quadtree.
+#' Smaller values (7-10) improve speed by bounding the recursion depth at the cost of accuracy.
+#' @param leaf.approximation Logical scalar indicating whether to use the \dQuote{leaf approximation}.
+#' If \code{TRUE}, repulsive forces are computed between leaf nodes and re-used across all cells assigned to that leaf node. 
+#' This sacrifices some accuracy for greater speed, assuming that \code{max.depth} is small enough for multiple cells to be assigned to the same leaf.
 #' @param seed Integer scalar specifying the seed to use for generating the initial coordinates.
 #' @param num.threads Integer scalar specifying the number of threads to use.
 #' @param max.iterations Integer scalar specifying the maximum number of iterations to perform.
+#' Larger values improve convergence at the cost of compute time.
 #' @param BNPARAM A \link[BiocNeighbors]{BiocNeighborParam} object specifying the algorithm to use.
 #' Only used if \code{x} is not a prebuilt index or a list of existing nearest-neighbor search results.
 #' 
@@ -40,9 +43,28 @@
 #' embedding <- runTsne(x)
 #' plot(embedding[,1], embedding[,2], col=iris[,5])
 #' 
+#' @references
+#' van der Maaten LJP and Hinton GE (2008). 
+#' Visualizing high-dimensional data using t-SNE. 
+#' \emph{Journal of Machine Learning Research_} 9, 2579-2605.
+#' 
+#' van der Maaten LJP (2014). 
+#' Accelerating t-SNE using tree-based algorithms. 
+#' \emph{Journal of Machine Learning Research} 15, 3221-3245.
+#' 
 #' @export
 #' @importFrom BiocNeighbors findKNN AnnoyParam
-runTsne <- function(x, perplexity=30, num.neighbors=tsnePerplexityToNeighbors(perplexity), max.depth=20, leaf.approximation=FALSE, max.iterations=500, seed=42, num.threads=1, BNPARAM=AnnoyParam()) {
+runTsne <- function(
+    x,
+    perplexity=30,
+    num.neighbors=tsnePerplexityToNeighbors(perplexity),
+    max.depth=20,
+    leaf.approximation=FALSE,
+    max.iterations=500,
+    seed=42,
+    num.threads=1,
+    BNPARAM=AnnoyParam())
+{
     if (!is.list(x)) {
         x <- findKNN(x, k=num.neighbors, transposed=TRUE, get.index="transposed", get.distance="transposed", num.threads=num.threads, BNPARAM=BNPARAM)
     } else {
